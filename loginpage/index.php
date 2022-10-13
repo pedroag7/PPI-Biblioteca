13dfe011ff4cede98a2ded7b037d05dfa9b45c62
Pedro A <pedroaggallina@gmail.com>
2022-10-13 15:30:37 -0300

    v13.10

--- loginpage/index.php
+++ loginpage/index.php
@@ -28,59 +28,59 @@
 </head>
 
 <body class="bg-green-600 font-roboto">
-    <div class="">
-    <div>
-        <h2 class=" flex-inline text-right pt-40 text-4xl font-bold tracking-tight text-white sm:pr-48 lg:pr-56 ">Sistema De Biblioteca Institucional</h2>
-        <iconify-icon icon="file-icons:owl" style="color: #fefefe;"></iconify-icon>
-     </div>
-        <div class="flex items-center justify-left pb-72 pt-32 sm:pl-48 lg:pl-56  max-w-screen-lg " id="formdiv">
-            <div class=" w-80  max-w-md space-y-8">
-                <div>
-                    
-                    <h2 class="mt-6 text-center text-5xl font-bold tracking-tight text-black">Seja Bem Vindo</h2>
-                    <p class="mt-2 text-center text-sm text-gray-600">
-                    </p>
-                </div>
-                <form class="mt-8 space-y-6 border-1" action="#" method="POST">
-                    <input type="hidden" name="remember" value="true">
-                    <div class="-space-y-px rounded-md shadow-sm">
-                        <div>
-                            <label for="email-address" class="sr-only">Email address</label>
-                            <input id="email-address" name="email" type="email" autocomplete="email" required class="relative block w-full appearance-none  rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm drop-shadow" placeholder="Login">
-                        </div>
-                        <br>
-                        <div>
-                            <label for="password" class="sr-only">Password</label>
-                            <input id="password" name="password" type="password" autocomplete="current-password" required class="relative block w-full appearance-none  rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm drop-shadow" placeholder="Senha">
-                        </div>
-                    </div>
 
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center">
-                            <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
-                            <label for="remember-me" class="ml-2 block text-sm text-gray-900">Lembrar-me</label>
-                        </div>
+    <div class=" flex-inline items-center pb-72 pt-32 sm:pl-40 lg:pl-48">
+        <div class="">
+            <h4 class="text-right justify-right text-4xl text-white pr-16 tracking-tight">Sistema de Biblioteca Institucional</h4>
+        </div>
+        <div class=" w-80 space-y-8">
 
-                        <div class="text-sm">
-                            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Esqueceu sua senha?</a>
-                        </div>
-                    </div>
-
-                    <div>
-                        <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-green-600 py-2 px-4 text-sm font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 shadow-md shadow-green-400/50 ">
-                            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-                                <!-- Heroicon name: mini/lock-closed -->
-                                <svg class="h-5 w-5 text-white group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
-                                </svg>
-                            </span>
-                            Fazer Login
-                        </button>
-                    </div>
-                </form>
+            <div>
+                <h2 class=" text-center text-5xl font-bold tracking-tight text-black">Seja Bem Vindo</h2>
+                <p class=" text-center text-sm text-gray-600">
+                </p>
             </div>
+
+            <form class="mt-8 space-y-6 border-1" action="#" method="POST">
+                <input type="hidden" name="remember" value="true">
+                <div class="-space-y-px rounded-md shadow-sm">
+                    <div>
+                        <label for="email-address" class="sr-only">Email address</label>
+                        <input id="email-address" name="email" type="email" autocomplete="email" required class="relative block w-80 appearance-none  rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm drop-shadow" placeholder="Login">
+                    </div>
+                    <br>
+                    <div>
+                        <label for="password" class="sr-only">Password</label>
+                        <input id="password" name="password" type="password" autocomplete="current-password" required class="relative block w-80 appearance-none  rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm drop-shadow" placeholder="Senha">
+                    </div>
+                </div>
+
+                <div class="flex items-center justify-between">
+                    <div class="flex items-center">
+                        <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
+                        <label for="remember-me" class="ml-2 block text-sm text-gray-900">Lembrar-me</label>
+                    </div>
+
+                    <div class="text-sm">
+                        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Esqueceu sua senha?</a>
+                    </div>
+                </div>
+
+                <div>
+                    <button type="submit" class="group relative flex w-80 justify-center rounded-md border border-transparent bg-green-600 py-2 px-4 text-sm font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 shadow-md shadow-green-400/50 ">
+                        <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                            <!-- Heroicon name: mini/lock-closed -->
+                            <svg class="h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
+                            </svg>
+                        </span>
+                        Fazer Login
+                    </button>
+                </div>
+            </form>
         </div>
     </div>
+
 
 </body>
 
